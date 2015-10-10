07d8a0ce922b7ae9e81e309c2cb64cc488deebf9
Anju <Anju@Anjus-MacBook-Air.local>
2015-10-09 23:07:11 -0700

    add js and css files

---
+++ js/jquery.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var favSongInput = $("input#favSong").val();
+    var favFoodInput = $("input#favFood").val();
+    var favDestinationInput = $("input#favDestination").val();
+    var favColorInput = $("input#favColor").val();
+
+    $(".name").text(nameInput);
+    $(".favSong").text(favColorInput);
+    $(".favFood").text(favFoodInput);
+    $(".favDestination").text(favDestinationInput);
+    $(".favColor").text(favColorInput);
+
+    $("#favoriteList").show();
+    event.preventDefault();
+  });
+});
